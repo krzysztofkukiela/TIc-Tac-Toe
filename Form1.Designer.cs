bae9087bf00319b028569185e3e8bb8553875a2c
krzysztofkukiela <57131137+krzysztofkukiela@users.noreply.github.com>
2022-02-08 15:12:24 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.singleplayer = new System.Windows.Forms.RadioButton();
+            this.multiplayer = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(108, 69);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(70, 70);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.Location = new System.Drawing.Point(188, 69);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(70, 70);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button3.Location = new System.Drawing.Point(268, 69);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(70, 70);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button4.Location = new System.Drawing.Point(108, 149);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(70, 70);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button5.Location = new System.Drawing.Point(188, 149);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(70, 70);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button6.Location = new System.Drawing.Point(268, 149);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(70, 70);
+            this.button6.TabIndex = 5;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button7.Location = new System.Drawing.Point(108, 229);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(70, 70);
+            this.button7.TabIndex = 6;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button8.Location = new System.Drawing.Point(188, 229);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(70, 70);
+            this.button8.TabIndex = 7;
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button9.Location = new System.Drawing.Point(268, 229);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(70, 70);
+            this.button9.TabIndex = 8;
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // singleplayer
+            // 
+            this.singleplayer.AutoSize = true;
+            this.singleplayer.Location = new System.Drawing.Point(441, 118);
+            this.singleplayer.Name = "singleplayer";
+            this.singleplayer.Size = new System.Drawing.Size(79, 21);
+            this.singleplayer.TabIndex = 9;
+            this.singleplayer.Text = "1 Gracz";
+            this.singleplayer.UseVisualStyleBackColor = true;
+            // 
+            // multiplayer
+            // 
+            this.multiplayer.AutoSize = true;
+            this.multiplayer.Location = new System.Drawing.Point(441, 149);
+            this.multiplayer.Name = "multiplayer";
+            this.multiplayer.Size = new System.Drawing.Size(86, 21);
+            this.multiplayer.TabIndex = 10;
+            this.multiplayer.Text = "2 Graczy";
+            this.multiplayer.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(837, 489);
+            this.Controls.Add(this.multiplayer);
+            this.Controls.Add(this.singleplayer);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.RadioButton singleplayer;
+        private System.Windows.Forms.RadioButton multiplayer;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Button[] btn = new Button[9];
+
+        public int[] horizontal = new int[3] { 0, 0, 0};
+        public int[] vertical = new int[3] { 0, 0, 0 };
+        public int[] diagonal = new int[2] { 0, 0 };
+
+        public bool round = false;
+        public bool AI_turn = false;
+        public int btn_counter = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            btn[0] = button1;
+            btn[1] = button2;
+            btn[2] = button3;
+            btn[3] = button4;
+            btn[4] = button5;
+            btn[5] = button6;
+            btn[6] = button7;
+            btn[7] = button8;
+            btn[8] = button9;
+        }
+        void AI_move(string line, int coordinates)
+        {
+            if (line == "brak")
+            {
+                Random rnd = new Random();
+                int box = rnd.Next(8);
+                int direction = rnd.Next(1, 3);
+                while (btn[box].Enabled != true)
+                {
+                    if (direction == 1)
+                    {
+                        if (box == 9)
+                            box = -1;
+                        ++box;
+                    }
+                    else
+                    {
+                        if (box == 0)
+                            box = 9;
+
+                        --box;
+                    }
+                }
+                action(box);
+            }
+            else
+            {
+                switch (line)
+                {
+                    case "horizontal":
+                        coordinates *= 3;
+                        while (btn[coordinates-1].Enabled == false)
+                            --coordinates;
+                        action(coordinates-1);
+                        break;
+                    case "vertical":
+                        while (btn[coordinates-1].Enabled == false)
+                            coordinates +=3;
+                        action(coordinates-1);
+                        break;
+                    case "diagonal":
+                        int tmp = 5;
+                        while (btn[tmp - 1].Enabled == false)
+                        {
+                            tmp += coordinates;
+                            coordinates = coordinates * -2;
+                        }
+                        action(tmp-1);
+                        break;
+                }
+            }
+        }
+        void reset()
+        {
+
+            Array.Clear(horizontal, 0, 3);
+            Array.Clear(vertical, 0, 3);
+            Array.Clear(diagonal, 0, 2);
+            int number = 9;     //ilość pól
+            for(int i = 0; i < number; ++i)
+            {
+                btn[i].Enabled = true;
+                btn[i].Text = " ";
+            }
+
+            round = false;
+            btn_counter = 0;
+
+        /*Form1 NewForm = new Form1();
+        NewForm.Show();
+        this.Dispose(false);*/
+    }
+        void end(int result)
+        {
+            string message;
+            string tittle = "Koniec";
+            if (result == 1)
+                message = "Wygrywa X";
+            else if (result == 2)
+                message = "Wygrywa O";
+            else
+                message = "Remis";
+            MessageBoxButtons buttons = MessageBoxButtons.RetryCancel;
+            DialogResult D_result = MessageBox.Show(message, tittle, buttons);
+            if (D_result == DialogResult.Retry)
+                reset();
+            else
+                this.Close();
+        }
+        void check(int sum)
+        {
+            if (sum == 3)
+                end(1);
+            else if (sum == -3)
+                end(2);
+            else if (btn_counter == 9)
+                end(0);
+        }
+        private void button_action(Button button, int horizontal, int vertical, int diagonal)
+        {
+            button.Enabled = false;
+            ++btn_counter;
+            if(round == false)
+            {
+                button.Text = "X";
+                if (horizontal > 0)
+                    check(++this.horizontal[horizontal - 1]);
+                if (vertical > 0) 
+                    check(++this.vertical[vertical-1]);
+                if (diagonal > 0 && diagonal < 3) 
+                    check(++this.diagonal[diagonal-1]);
+                else if (diagonal == 3)
+                {
+                   check(++this.diagonal[0]);
+                   check(++this.diagonal[1]);
+                }
+            }
+            else
+            {
+                button.Text = "O";
+                if (horizontal > 0)
+                    check(--this.horizontal[horizontal - 1]);
+                if (vertical > 0)
+                    check(--this.vertical[vertical - 1]);
+                if (diagonal > 0 && diagonal < 3)
+                    check(--this.diagonal[diagonal - 1]);
+                else if (diagonal == 3)
+                {
+                    check(--this.diagonal[0]);
+                    check(--this.diagonal[1]);
+                }
+            }
+            round = !round;
+            AI_turn = !AI_turn;
+            if (singleplayer.Checked == true && AI_turn == true)
+            {
+                string line = "brak";
+                int coordinates = 0;
+                if (this.horizontal[horizontal - 1] == 2 || this.horizontal[horizontal - 1] == -2)
+                {
+                    line = "horizontal";
+                    coordinates = horizontal;
+                }
+                if (this.vertical[vertical - 1] == 2 || this.vertical[vertical - 1] == 2)
+                {
+                    line = "vertical";
+                    coordinates = vertical;
+                }
+                if (this.diagonal[0] == 2 || this.diagonal[0] == -2)
+                {
+                    line = "diagonal";
+                    coordinates = 4;
+                }
+                if (this.diagonal[1] == 2 || this.diagonal[1] == -2)
+                {
+                    line = "diagonal";
+                    coordinates = 1;
+                }
+                AI_move(line, coordinates);
+            }
+        }
+        private void action(int a)
+        {
+            switch(a)
+            {
+                case 0:
+                    button_action(btn[a], 1, 1, 1);
+                    break;
+                case 1:
+                    button_action(btn[a], 1, 2, 0);
+                    break;
+                case 2:
+                    button_action(btn[a], 1, 3, 2);
+                    break;
+                case 3:
+                    button_action(btn[a], 2, 1, 0);
+                    break;
+                case 4:
+                    button_action(btn[a], 2, 2, 3);
+                    break;
+                case 5:
+                    button_action(btn[a], 2, 3, 0);
+                    break;
+                case 6:
+                    button_action(btn[a], 3, 1, 2);
+                    break;
+                case 7:
+                    button_action(btn[a], 3, 2, 0);
+                    break;
+                case 8:
+                    button_action(btn[a], 3, 3, 1);
+                    break;
+            }
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            action(0);
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            action(1);
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            action(2);
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            action(3);
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            action(4);
+        }
+        private void button6_Click(object sender, EventArgs e)
+        {
+            action(5);
+        }
+        private void button7_Click(object sender, EventArgs e)
+        {
+            action(6);
+        }
+        private void button8_Click(object sender, EventArgs e)
+        {
+            action(7);
+        }
+        private void button9_Click(object sender, EventArgs e)
+        {
+            action(8);
+        }
+    }
+}
